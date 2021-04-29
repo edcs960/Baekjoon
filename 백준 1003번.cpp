4adfd8aee583d38d640c7c3cc517f672422a582f
edcs960 <65010544+edcs960@users.noreply.github.com>
2021-04-29 11:27:52 +0900

    백준 1003번 문제
    피보나치 함수에서 0이 들어갔을 때의 개수와 1이 들어갔을 때의 개수를 출력하는 문제입니다.

--- 백준 1003번.cpp
+++ 백준 1003번.cpp
@@ -1,20 +1,16 @@
 #include<stdio.h>
 
-int dp[41] = {0,1,1};
+int main(){
+    int T=0,N=0;
+	int dp[41] = {0,1,1};
 
-void fibo(int n){
+    scanf("%d",&T);
+
 	for(int i = 3;i<41;i++){
 		dp[i] = dp[i-1] + dp[i-2];
 	}
-}
 
-int main(){
-    int T=0,N=0;
-    scanf("%d",&T);
-	fibo(41);
-
-    while(T>=0){
-		printf("%d \n",T);
+    while(T>0){
 		scanf("%d",&N);
 		if(N==0) printf("1 0\n");
 		else if(N==1) printf("0 1\n");
