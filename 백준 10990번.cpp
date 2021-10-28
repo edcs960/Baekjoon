ce6fef42514e80a9127c2d4c1ef42548a1759f4c
edcs960 <65010544+edcs960@users.noreply.github.com>
2021-10-28 14:11:02 +0900

    백준 10990번 문제
    별찍기 - 15

---
+++ 백준 10990번.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+
+	scanf("%d",&n);
+
+	if(n==1){printf("*");}
+	else{
+		for(int i = 0; i<n;i++){
+			for(int j = i; j<n-1;j++){printf(" ");}
+			for(int j = 0; j<2*i+1;j++){
+				if(i==0){printf("*"); break;}
+				else if(j==0 || j == 2*i){printf("*");}
+				else{printf(" ");}
+			}
+			printf("\n");
+		}
+	}
+}
