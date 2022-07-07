c14ac7d5f1d8af8b3fe9618d97b0e1935f991213
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-07 19:17:13 +0900

    백준 1193번
    -

---
+++ 백준 1193번.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string X = Console.ReadLine();
+            int x = int.Parse(X);
+            int sum = 0;
+            int i = 1;
+            
+            int a, b;
+
+            if(int.Parse(X) == 1)
+            {
+                a = 1;
+                b = 1;
+            }
+            else
+            {
+                int tmp = x;
+                while (x > sum)
+                {
+                    sum += i;
+                    if (tmp > i)
+                    {
+                        tmp = tmp - i;
+                        i++;
+                    }
+                }
+
+                if (i % 2 == 0)
+                {
+                    a = 1;
+                    b = i;
+                }
+                else
+                {
+                    a = i;
+                    b = 1;
+                }
+
+                for (int j = 1; j < tmp; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        a += 1;
+                        b -= 1;
+                    }
+                    else
+                    {
+                        a -= 1;
+                        b += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Format("{0}/{1}",a,b));
+        }
+    }
+}
