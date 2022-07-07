22a21852d2a716b155ba31ed71bc3fa3d633fbfb
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-07 11:13:19 +0900

    백준 2292번
    1 - 7 - 19 - ... 로 늘어나는 것을 확인, 1단계씩 넘어갈수록 6의 배수로 늘어나 공식을 세울수 있음.
    그리고 최소로 통과하는 방의 개수를 찾기 위해서 위 공식을 이용해 입력받은 숫자가 작을 때까지 반복문을 돌려서 찾을수있음.

---
+++ 백준 2292번.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int i = 0;
+            int sum = 1;
+
+            if (int.Parse(num) == 1)
+            {
+                i = 1;
+            }
+            else
+            {
+                while (int.Parse(num) > sum)
+                {
+
+                    sum += (6 * i);
+                    i++;
+                }
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
