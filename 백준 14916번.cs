0240d7d98cb6f81bc44edf19ad6382e902b182a6
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-11 11:12:17 +0900

    백준 14916번
    -

---
+++ 백준 14916번.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            if(n == 1 || n == 3)
+            {
+                count = -1;
+            }
+            else
+            {
+                while(n > 0)
+                {
+                    if (n % 5 == 0)
+                    {
+                        n -= 5;
+                        count++;
+                    }
+                    else
+                    {
+                        n -= 2;
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
