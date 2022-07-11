52fd4f63a070b9f0dbea90a3181ecc14421a809b
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-11 10:39:16 +0900

    백준 1292번
    -

---
+++ 백준 1292번.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split(' ');
+            int sum = 0;
+            List<int> arr = new List<int>();
+
+            for(int i = 1; i<= 1000; i++)
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    arr.Add(i);
+                }
+            }
+
+            for(int i = int.Parse(a[0])-1;i <= int.Parse(a[1]) - 1; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
