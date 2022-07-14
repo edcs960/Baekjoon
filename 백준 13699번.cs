89d0ccb818e5c2995cf6456245b0b3fa1d6cec4b
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-14 15:25:56 +0900

    백준 13699번
    -

---
+++ 백준 13699번.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[] dp = new long[36];
+
+            dp[0] = 1;
+            for(int i = 1; i <= 35; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    dp[i] += dp[j] * dp[i - j - 1];
+                }
+            }
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
