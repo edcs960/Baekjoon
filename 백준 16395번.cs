24e76e25ccc59632dc1eb603ae1d62a1f76d640d
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-14 15:46:50 +0900

    백준 16395번
    -

---
+++ 백준 16395번.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(input.Split(' ')[0]);
+            int k = int.Parse(input.Split(' ')[1]);
+            int[,] pascal = new int[30, 30];
+
+            for(int i = 0; i < 30; i++)
+            {
+                for(int j = 0; j < 30; j++)
+                {
+                    if (i == j)
+                    {
+                        pascal[i, j] = 1;
+                        break;
+                    }
+                    else if (j == 0)
+                    {
+                        pascal[i, j] = 1;
+                        continue;
+                    }
+                    else
+                    {
+                        pascal[i, j] = pascal[i - 1, j - 1] + pascal[i - 1, j];
+                    }
+                }
+            }
+            Console.WriteLine(pascal[n-1, k-1]);
+        }
+    }
+}
