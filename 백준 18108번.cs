963a1a639e706325ea3693f67258105c69ae5153
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-26 18:12:03 +0900

    백준 18108번

---
+++ 백준 18108번.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()) - 543);
+        }
+    }
+}
