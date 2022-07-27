c3b64c1afcc93245152eef8531820cf466650e4f
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-27 17:42:42 +0900

    백준 2420번
    백준 2420번

---
+++ 백준 2420번.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n = Console.ReadLine().Split(' ');
+            Console.WriteLine(Math.Abs(long.Parse(n[0]) - long.Parse(n[1])));
+        }
+    }
+}
