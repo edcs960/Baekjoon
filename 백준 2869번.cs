38cb9d82ff7b670cc656a264a7e3ef63867bb77a
edcs960 <65010544+edcs960@users.noreply.github.com>
2022-07-27 18:25:41 +0900

    백준 2869번
    백준 2869번

---
+++ 백준 2869번.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Basic_Cshap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int V = int.Parse(input[2]);
+            int result = 1;
+            if (V == A)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                if((V-A) % (A-B) == 0)
+                {
+                    result += (V - A) / (A - B);
+                }
+                else
+                {
+                    result += (V - A) / (A - B) + 1;
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
